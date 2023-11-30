f97bd289d3ac101a410cf9093ca8daac1ffedb29
poutinedude <priggen@educ.college-montreal.qc.ca>
2023-11-30 18:51:08 -0500

    ajout du running average dans main.ino

--- main/main.ino
+++ main/main.ino
@@ -1,25 +1,58 @@
+const byte trigPin = 12; // Déclaration des 2 broches du capteur
+const byte echoPin = 7;
 
-const int trigPin = 9;
-const int echoPin = 10;
+//variables de programme
+long distance_cm = 0;
+long intervalle_temps = 0;
+int liste[10];
+byte last_position = 0;
+long distance_average = 0;
 
-float duration, distance;
 
-void setup() {
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-  Serial.begin(9600);
+// fonction de calcul de la moyenne
+float running_average()
+{
+    float moyenne = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        moyenne += liste[i];
+    }
+    moyenne = moyenne / 10;
+    return moyenne;
 }
 
-void loop() {
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
+void setup() 
+{
+	Serial.begin (9600);
+	pinMode(trigPin, OUTPUT);
+	pinMode(echoPin, INPUT);
+}
 
-  duration = pulseIn(echoPin, HIGH);
-  distance = (duration*.0343)/2;
-  Serial.print("Distance: ");
-  Serial.println(distance);
-  delay(100);
+
+void loop() 
+{
+	// le senseur est activé par un pulse HIGH d’au moins 10 microsecondes.
+	// à la reception de ce signal il envoie une série de 8 ondes sonores (40KHz) (i.e. ultrason)
+	digitalWrite(trigPin, LOW); // reset du trigPin pour la validité du signal HIGH (excès de zèle?)
+	delayMicroseconds(2);
+	digitalWrite(trigPin, HIGH);
+	delayMicroseconds(10);
+	digitalWrite(trigPin, LOW);
+
+	// lecture de la réponse
+	intervalle_temps = pulseIn(echoPin, HIGH); // Temps en µs séparant l&#39;envoi de la réception
+
+	distance_cm = (intervalle_temps * 0.03435) / 2;
+	liste[last_position] = distance_cm;
+	distance_average = running_average();
+
+	last_position++;
+	if (last_position == 10)
+	{
+		last_position = 0;
+	}
+
+
+	Serial.println(distance_average);
+	delay(50); // 1/20 seconde sépare chaque prise de mesure
 }
